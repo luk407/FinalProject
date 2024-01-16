24c900bb14f0a0f0dd756d6a0b40583d577f12a9
luk407 <luka.gazdeliani07@gmail.com>
2024-01-16 20:57:23 +0400

    initial commit

---
+++ BookSmart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookSmart
+//
+//  Created by Luka Gazdeliani on 16.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
