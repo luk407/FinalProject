d032d408e3f087afa238494906489124a94b1cc9
luk407 <luka.gazdeliani07@gmail.com>
2024-02-09 22:45:14 +0400

    fix problem with fetching incorrect info for comment author

--- BookSmart/Scenes/Home/PostDetailsScene/CommentTableViewCell/CommentTableViewCell.swift
+++ BookSmart/Scenes/Home/PostDetailsScene/CommentTableViewCell/CommentTableViewCell.swift
@@ -394,157 +394,19 @@ class CommentTableViewCell: UITableViewCell {
     }
     
     func getAuthorInfo(with authorID: UserInfo.ID, completion: @escaping (UserInfo?) -> Void) {
-        
-        var authorInfo: UserInfo?
-        
-        let database = Firestore.firestore()
-        let reference = database.collection("UserInfo")
-        
-        reference.getDocuments { [weak self] snapshot, error in
-            
-            guard let self = self else { return }
-            
-            if let error = error {
-                print("Error fetching user information: \(error.localizedDescription)")
-                completion(nil)
-                return
-            }
-            
-            guard let snapshot = snapshot else {
-                completion(nil)
-                return
-            }
-            
-            for document in snapshot.documents {
-                let data = document.data()
-                
-                guard
-                    let id = data["id"] as? String,
-                    let username = data["username"] as? String,
-                    let email = data["email"] as? String,
-                    let password = data["password"] as? String,
-                    let displayName = data["displayName"] as? String,
-                    let registrationDateTimestamp = data["registrationDate"] as? Timestamp,
-                    let bio = data["bio"] as? String,
-                    let image = data["image"] as? String,
-                    let badgesData = data["badges"] as? [[String: String]],
-                    let posts = data["posts"] as? [String],
-                    let comments = data["comments"] as? [String],
-                    let likedPosts = data["likedPosts"] as? [String],
-                    let connections = data["connections"] as? [String],
-                    let booksFinishedArray = data["booksFinished"] as? [[String: Any]],
-                    let quotesUsedData = data["quotesUsed"] as? [[String: String]]
-                else {
-                    print("Error parsing user data")
-                    completion(nil)
-                    continue
-                }
-                
-                let badges = self.parseBadgesArray(badgesData)
-                let quotesUsed = self.parseQuotesArray(quotesUsedData)
-                let booksFinished = self.parseBooksFinishedArray(booksFinishedArray)
-                
-                let userInfo = UserInfo(
-                    id: UUID(uuidString: id) ?? UUID(),
-                    userName: username,
-                    email: email,
-                    password: password,
-                    displayName: displayName,
-                    registrationDate: registrationDateTimestamp.dateValue(),
-                    bio: bio,
-                    image: image,
-                    badges: badges,
-                    posts: posts.map { UUID(uuidString: $0) ?? UUID() },
-                    comments: comments.map { UUID(uuidString: $0) ?? UUID() },
-                    likedPosts: likedPosts.map { UUID(uuidString: $0) ?? UUID() },
-                    connections: connections.map { UUID(uuidString: $0) ?? UUID() },
-                    booksFinished: booksFinished,
-                    quotesUsed: quotesUsed
-                )
-                authorInfo = userInfo
-            }
-            
+        FirebaseManager.shared.getAuthorInfo(with: authorID) { authorInfo in
             completion(authorInfo)
         }
-    }
-    
-    private func parseBooksFinishedArray(_ booksFinishedArray: [[String: Any]]) -> [Book] {
-        var booksFinished: [Book] = []
-        
-        for bookInfo in booksFinishedArray {
-            if let title = bookInfo["title"] as? String,
-               let authorName = bookInfo["authorName"] as? [String] {
-                let book = Book(title: title, authorName: authorName)
-                booksFinished.append(book)
-            }
-        }
-        return booksFinished
-    }
-    
-    private func parseBadgesArray(_ badgesData: [[String: String]]) -> [BadgeInfo] {
-        
-        var badges: [BadgeInfo] = []
-        
-        for badgeInfo in badgesData {
-            if
-                let categoryString = badgeInfo["category"],
-                let category = BadgeCategory(rawValue: categoryString),
-                let typeString = badgeInfo["type"],
-                let type = BadgeType(rawValue: typeString)
-            {
-                let badge = BadgeInfo(category: category, type: type)
-                badges.append(badge)
-            }
-        }
-        return badges
-    }
-    
-    private func parseQuotesArray(_ quotesData: [[String: String]]) -> [Quote] {
-        var quotes: [Quote] = []
-        
-        for quoteData in quotesData {
-            if let text = quoteData["text"], let author = quoteData["author"] {
-                let quote = Quote(text: text, author: author)
-                quotes.append(quote)
-            }
-        }
-        
-        return quotes
     }
     
     func retrieveImage() {
         authorImageView.image = UIImage(systemName: "person.fill")
         authorImageView.tintColor = .customAccentColor
         
-        guard let imageName = commentInfo?.authorID.uuidString else { return }
+        guard let authorIDString = commentInfo?.authorID.uuidString else { return }
         
-        if let cachedImage = CacheManager.instance.get(name: imageName) {
-            authorImageView.image = cachedImage
-        } else {
-            let database = Firestore.firestore()
-            database.collection("UserInfo").document((commentInfo?.authorID.uuidString)!).getDocument { document, error in
-                if error == nil && document != nil {
-                    let imagePath = document?.data()?["image"] as? String
-                    self.fetchImage(imagePath ?? "")
-                }
-            }
-        }
-    }
-    
-    private func fetchImage(_ imagePath: String) {
-        let storageReference = Storage.storage().reference()
-        let fileReference = storageReference.child(imagePath)
-        
-        fileReference.getData(maxSize: 5 * 1024 * 1024) { data, error in
-            if let data = data, error == nil, let fetchedImage = UIImage(data: data) {
-                print("Image fetched successfully.")
-                DispatchQueue.main.async {
-                    self.authorImageView.image = fetchedImage
-                    CacheManager.instance.add(image: fetchedImage, name: self.commentInfo?.authorID.uuidString ?? "")
-                }
-            } else {
-                print("Error fetching image:", error?.localizedDescription ?? "Unknown error")
-            }
+        FirebaseManager.shared.retrieveImage(authorIDString) { commentAuthorImage in
+            self.authorImageView.image = commentAuthorImage
         }
     }
 }
